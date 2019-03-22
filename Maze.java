c5c561282e1f1bae0f94e41d152428b647c23f40
jxw1070 <jxw1070@cs.bham.ac.uk>
2019-03-22 16:57:54 +0000

    Upload New File

---
+++ Maze.java
@@ -0,0 +1,51 @@
+package ijev3as2;
+
+
+import java.util.Arrays;
+import java.util.*;
+
+import lejos.hardware.lcd.LCD;
+
+public class Maze {
+
+    Node[][] nodes;
+    Set<Node> unvisited = new HashSet<>();
+    Set<Node> visited = new HashSet<>();
+    public Maze(int length, int width) {
+        this.nodes = new Node[length][width];
+        initializeMaze();
+    }
+
+	private void initializeMaze() {
+		for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 13; j++) {
+                nodes[i][j] = new Node(i, j);
+                nodes[i][j].setCondition(-1);
+                unvisited.add(nodes[i][j]);
+            }
+        }
+		nodes[1][1].setCondition(0);
+	}
+
+    public void printMz() {
+    	initializeMaze();
+    	int k = 0;
+    	int l = 0;
+    	for (int i = 0; i < nodes.length; i+=1) {
+            for (int j = 0; j < nodes[i].length; j+=1) {
+            	
+                LCD.setPixel(k, l, 1);
+                LCD.setPixel(k+1, l, 1);
+                //LCD.setPixel(i+2, j, 1);
+                LCD.setPixel(k, l+1, 1);
+                LCD.setPixel(++k, ++l, 1);
+                //LCD.setPixel(i, j, 1);
+                
+            }
+           
+        }
+
+    }
+
+}
+
