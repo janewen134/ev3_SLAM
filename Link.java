8855e632e542c587aa2d6ab27b2504f7eafa4e41
jxw1070 <jxw1070@cs.bham.ac.uk>
2019-03-22 16:57:34 +0000

    Upload New File

---
+++ Link.java
@@ -0,0 +1,14 @@
+package ijev3as2;
+
+public class Link {
+	Node from;
+	Node to;
+	boolean skip;
+	public Link(Node from, Node to) {
+		//super();
+		this.from = from;
+		this.to = to;
+		this.skip = false;
+	}
+	
+}
